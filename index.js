d3d20604d73190e369720f770be350ab3cef145c
Hoshimikan6490 <creative6490@gmail.com>
2023-09-11 18:42:42 +0900

    挫けた

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
-const { request } = require("undici");
 const express = require("express");
-require("dotenv").config();
-
+const axios = require("axios");
 const app = express();
+const path = require("path");
+require("dotenv").config();
 
 let public_id = process.env.clientId;
 let private_id = process.env.clientSecret;
-let port = 8000;
 
-app.get("/", async ({ query }, response) => {
-  const { code } = query;
-
-  if (code) {
-    try {
-      const tokenResponseData = await request(
-        "https://discord.com/api/oauth2/token",
-        {
-          method: "POST",
-          body: new URLSearchParams({
-            client_id: public_id,
-            client_secret: private_id,
-            code,
-            grant_type: "authorization_code",
-            redirect_uri: `http://localhost:${port}`,
-            scope: "identify",
-          }).toString(),
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      );
-
-      const oauthData = await tokenResponseData.body.json();
-
-      const userResult = await request("https://discord.com/api/users/@me", {
-        headers: {
-          authorization: `${oauthData.token_type} ${oauthData.access_token}`,
-        },
-      });
-
-      console.log(await userResult.body.json());
-    } catch (error) {
-      // NOTE: An unauthorized token will not throw an error
-      // tokenResponseData.statusCode will be 401
-      console.error(error);
-    }
-  }
-
-  return response.sendFile("index.html", { root: "." });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${port}`)
-);
+app.get("/login", (req, res) => {
+  const redirect_uri = encodeURIComponent("http://localhost:3000/auth");
+  res.redirect(
+    `https://discord.com/api/oauth2/authorize?client_id=${public_id}&redirect_uri=${redirect_uri}&response_type=code&scope=identify%20guilds`
+  );
+});
+
+app.get("/auth", async (req, res) => {
+  const code = req.query.code;
+  const data = {
+    client_id: public_id,
+    client_secret: private_id,
+    grant_type: "authorization_code",
+    code: code,
+    redirect_uri: "http://localhost:3000/auth",
+    scope: "identify guilds",
+  };
+
+  let response = await axios.post(
+    "https://discord.com/api/oauth2/token",
+    new URLSearchParams(data),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+
+  const token = response.data.access_token;
+  const guilds = await axios.get("https://discord.com/api/users/@me/guilds", {
+    headers: {
+      authorization: `${response.data.token_type} ${token}`,
+    },
+  });
+
+  res.send(guilds.data.map((guild) => guild.name).join("\n"));
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
